352c8a80c8182be3265f9f8953bd99e6cf7ea8f4
mokcheh <51539216+Mokcheh@users.noreply.github.com>
2024-08-24 20:01:11 +0100

    Add CPU class

---
+++ include/cpu.hpp
@@ -0,0 +1,26 @@
+#include <cstdint>
+
+
+struct Registers{
+    uint8_t
+        A,
+        F,
+        B,
+        C,
+        D,
+        E,
+        H,
+        L;
+};
+
+class LR35902{
+public:
+    LR35902();
+    ~LR35902();
+private:
+    Registers registers;
+    Registers alternatives;
+    uint16_t
+        stackPtr,
+        programCounter;
+};
---
+++ src/cpu.cpp
@@ -0,0 +1,10 @@
+#include "cpu.hpp"
+
+LR35902::LR35902(): registers({}), alternatives({})
+{
+}
+
+LR35902::~LR35902()
+{
+
+};
